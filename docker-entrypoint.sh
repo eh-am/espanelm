962277c724e4575ae86991c2aee9f1ff09508ab8
eduardo <eduardoaleixomartins@gmail.com>
2020-01-01 20:52:57 -0300

    remove acl from s3 cp cmd

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -22,9 +22,9 @@ echo "Validating JSON..."
 size=$(jq -e '. | length' < "news.json")
 if [[ "$size" -gt "0" ]]; then
 	echo "Syncing files..."
-	aws s3 cp --acl='public-read' news.json s3://espanelm-website/news.json
+	aws s3 cp news.json s3://espanelm-website/news.json
 
-	aws s3 cp --acl='public-read' news.json s3://espanelm-website/news_"$(date '+%Y-%m-%d_%H-%M-%S_%s')".json
+	aws s3 cp news.json s3://espanelm-website/news_"$(date '+%Y-%m-%d_%H-%M-%S_%s')".json
 else
 	echo "Invalid json. Won't sync."
 	exit 1
