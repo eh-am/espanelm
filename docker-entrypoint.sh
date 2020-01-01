a8f0cf03352c6ae2441a960396f07541e9f4521b
eduardo <eduardoaleixomartins@gmail.com>
2020-01-01 20:03:00 -0300

    scrape from multiple brazilian rss feeds

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,19 @@
 #!/bin/env bash
 
 echo "Fetching latest articles..."
-node /app/index.js "https://elpais.com/rss/elpais/portada.xml" > news.json
+
+# es feed, not that relevant since most articles have not been translated yet
+#node /app/index.js "https://elpais.com/rss/elpais/portada.xml" > news.json
+node /app/index.js "https://brasil.elpais.com/seccion/rss/internacional" > internacional.json
+node /app/index.js "https://brasil.elpais.com/seccion/rss/cultura" > cultura.json
+
+# there's not many articles there, so it may not be that relevant
+# node /app/index.js "https://brasil.elpais.com/seccion/rss/tecnologia" > tecnologia.json
+# node /app/index.js "https://brasil.elpais.com/seccion/rss/deportes" > deportes.json
+# node /app/index.js https://brasil.elpais.com/seccion/rss/ciencia > ciencia.json
+# node /app/index.js https://brasil.elpais.com/seccion/rss/opinion > opinion.json
+jq -s add internacional.json cultura.json > news.json
+
 echo "Displaying news.json"
 cat news.json
 
