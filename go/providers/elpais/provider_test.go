41e3a77a1265a1ee40c2e6d373cdc42938d80e43
eduardo <eduardoaleixomartins@gmail.com>
2021-05-15 23:31:43 -0300

    chore(go): add published time to test

--- go/providers/elpais/provider_test.go
+++ go/providers/elpais/provider_test.go
@@ -101,15 +101,18 @@ func TestElPaisProcessPage(t *testing.T) {
 		want elpais.ElPaisArticle
 	}
 
+	now := time.Now()
 	tc := []args{
 		{
 			page: elpais.Page{
-				Provider: "elpais", Links: []elpais.Link{
+				Published: &now,
+				Provider:  "elpais", Links: []elpais.Link{
 					{Url: "https://brasil.elpais.com/cultura/2021-05-09/agua-de-murta-o-desodorante-de-isabel-a-catolica.html", Lang: "pt-BR"},
 					{Url: "https://elpais.com/cultura/2021-05-07/agua-de-murta-el-desodorante-de-isabel-la-catolica.html", Lang: "es-ES"}},
 			},
 			want: elpais.ElPaisArticle{
-				Id: "4297f3862a64394ed1367885671c604c",
+				Id:        "4297f3862a64394ed1367885671c604c",
+				Published: &now,
 				PtBr: elpais.ReadableArticle{
 					Url:         "https://brasil.elpais.com/cultura/2021-05-09/agua-de-murta-o-desodorante-de-isabel-a-catolica.html",
 					Title:       "Água de murta, o desodorante de Isabel, a Católica",
